51cbd1d5c38412c4dbe3682c7d68b39dd9deca6a
Prathamesh patil <prathmesh8485049898@gmail.com>
2025-01-02 17:42:18 +0530

    Python Practice

---
+++ Assignments_Python/Day1_Assignment/Day1_Assignment.py
@@ -0,0 +1,15 @@
+#calculate percentage of students based on marks of any 5 subject 
+
+id = 106
+s1 = 74
+s2 = 85
+s3 = 44
+s4 = 52
+s5 = 65
+
+total = s1+s2+s3+s4+s5
+
+percentage = total / 5 
+
+print("Total percentage is the {0}".format(percentage))
+print(f"Total percentage is the {id} = {percentage}")
